3be3719a29c5efa9f209246bf4b8b3157c06e9fe
kpdecker <kpdecker@gmail.com>
2013-03-02 12:46:02 -0600

    Pass user agent to the instance

--- client-context.js
+++ client-context.js
@@ -15,7 +15,7 @@ module.exports = exports = function(options) {
     },
 
     navigator: {
-      userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_0 like Mac OS X; en-us) AppleWebKit/532.9 (KHTML, like Gecko) Version/4.0.5 Mobile/8A293 Safari/6531.22.7'
+      userAgent: options.userAgent
     },
 
     sessionStorage: {
