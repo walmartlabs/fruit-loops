15c0145d7041e6c81057120a989bbc6df7043202
kpdecker <kpdecker@gmail.com>
2013-05-15 18:39:40 -0500

    Navigator dom object

---
+++ test/dom/navigator.js
@@ -0,0 +1,13 @@
+var navigator = require('../../lib/dom/navigator');
+
+describe('dom - navigator', function() {
+  var window;
+  beforeEach(function() {
+    window = {};
+  });
+
+  it('should record user agent', function() {
+    navigator(window, {userAgent: 'foo'});
+    window.navigator.userAgent.should.equal('foo');
+  });
+});
