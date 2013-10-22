19f0c180ff11c4f38bee1b132fdad1fd5f187e0a
kpdecker <kpdecker@gmail.com>
2013-10-21 23:51:45 -0500

    Add dom.document tests

---
+++ test/dom/document.js
@@ -0,0 +1,46 @@
+var document = require('../../lib/dom/document');
+
+describe('dom.document', function() {
+  var window,
+      ret;
+  beforeEach(function() {
+    ret = [];
+    window = {
+      $: this.spy(function() { return ret; })
+    };
+    document(window);
+  });
+
+  it('should define proper fields', function() {
+    window.document.defaultView.should.equal(window);
+  });
+
+  describe('#body', function() {
+    it('should return the body query', function() {
+      ret = ['foo'];
+      window.document.body.should.equal('foo');
+      window.$.should.have.been.calledWith('body');
+    });
+  });
+  describe('#querySelector', function() {
+    it('should query arbitrary values', function() {
+      ret = ['foo'];
+      window.document.querySelector('bar').should.equal('foo');
+      window.$.should.have.been.calledWith('bar');
+    });
+  });
+  describe('#querySelectorAll', function() {
+    it('should query arbitrary values', function() {
+      ret = ['foo'];
+      window.document.querySelectorAll('bar').should.eql(['foo']);
+      window.$.should.have.been.calledWith('bar');
+    });
+  });
+  describe('#querySelectorAll', function() {
+    it('should query arbitrary values', function() {
+      ret = ['foo'];
+      window.document.createElement('bar').should.eql(['foo']);
+      window.$.should.have.been.calledWith('<bar>');
+    });
+  });
+});
