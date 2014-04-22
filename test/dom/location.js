e3e74d12779443768c499fdf9257cad1fe608b50
kpdecker <kpdecker@gmail.com>
2014-04-22 10:24:01 -0500

    Use empty string for empty location.search
    This matches the behavior of the browsers and avoids NPE cases for code not expecting Node's null value.

--- test/dom/location.js
+++ test/dom/location.js
@@ -14,6 +14,7 @@ describe('dom.location', function() {
     window.location.host.should.equal('foo.bar');
     window.location.path.should.equal('/foo/bar');
     window.location.origin.should.equal('http://foo.bar');
+    window.location.search.should.equal('?baz=bat&boz=');
     window.location.hash.should.equal('');
     (window.location+'').should.equal('http://foo.bar/foo/bar?baz=bat&boz=');
   });
@@ -22,6 +23,16 @@ describe('dom.location', function() {
     location(window, 'https://foo.bar:8080/foo/bar?baz=bat&boz=');
     window.location.host.should.equal('foo.bar:8080');
     window.location.origin.should.equal('https://foo.bar:8080');
+  });
+  it('should provide non-null failover', function() {
+    location.preInit(window);
+    location(window, 'http://foo.bar');
+    window.location.host.should.equal('foo.bar');
+    window.location.path.should.equal('/');
+    window.location.origin.should.equal('http://foo.bar');
+    window.location.search.should.equal('');
+    window.location.hash.should.equal('');
+    (window.location+'').should.equal('http://foo.bar/');
   });
 
   it('should redirect on assign()', function() {
